f8ede7a27812d1bb1a6c4c3a7d3c09565e2a69fd
mohits4 <mohits4@chetu.com>
2020-10-23 17:21:54 +0530

    active-inactive-ajax-change

--- resources/views/admin_deshboard.blade.php
+++ resources/views/admin_deshboard.blade.php
@@ -11,6 +11,9 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 </head>
 <body>
 
@@ -31,7 +34,10 @@
         <td>{{$user->name}}</td>
         <td>{{$user->email}}</td>
         
-        <td class="statusc" alt="{{$user->id}}"><a href="#">@if($user->status == 1) Active @else Deactivate @endif</a></td>
+        <!-- <td class="toggle-class" alt="{{$user->id}}"><a href="#">@if($user->status == 1) Active @else Deactivate @endif</a></td> -->
+        <td>
+    <input data-id="{{$user->id}}" class="toggle-class" type="checkbox" data-onstyle="success" data-offstyle="danger" data-toggle="toggle" data-on="Active" data-off="InActive" {{ $user->status ? 'checked' : '' }}>
+        </td>
       </tr>
     @endforeach
     </tbody>
@@ -43,21 +49,23 @@
 
 <script type="text/javascript">
   $(document).ready(function() {
-  $(".statusc").click(function()  {
+  $(".toggle-class").change(function()  {
     //alert('sagsd');
-    var id = $(this).attr("alt");
+    var id = $(this).attr("data-id");
     //var status = $("#activitymessage").val();
     var id = id;
     $.ajax({
       type: "get",
       url: "{{route("status")}}",
       data: {id: id},
-      success: function(msg) {
-        location.reload();
-      }
+      success: function(data){
+              console.log(data.success)
+            }
+      //       success: function(msg) {
+      //   location.reload();
+      // }
     });
    });
   });
 </script>
-
 @endsection
